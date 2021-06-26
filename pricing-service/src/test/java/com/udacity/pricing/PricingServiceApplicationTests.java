772af875779f19be25d0045a2cf1b6216442393a
João Calixto <calixtounicap@gmail.com>
2021-06-26 17:04:33 +0100

    Initial commit

---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,51 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	PricingService pricingService;
+
+	@Test
+	public void testGetPriceWithVeicleId() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(get("/services/price?vehicleId=7"))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String contentAsString = mvcResult.getResponse().getContentAsString();
+
+		assertTrue(contentAsString.isBlank());
+
+		verify(pricingService, times(1)).getPrice(7l);
+
+	}
+
+}
---
+++ pricing-service/src/test/java/com/udacity/pricing/service/PricingServiceTest.java
@@ -0,0 +1,12 @@
+package com.udacity.pricing.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PricingServiceTest {
+
+    @Test
+    public void getPrice() {
+    }
+}
